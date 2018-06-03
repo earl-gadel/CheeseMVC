25bd8c49d060a4d43701cd9fc3e40efc852c13ef
Earl <earljg@gmail.com>
2018-06-03 18:08:33 -0500

    created cheese constructor in model

--- CheeseMVC/Controllers/CheeseController.cs
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,7 +12,7 @@ namespace CheeseMVC.Controllers
     public class CheeseController : Controller
     {
 
-        static private Dictionary<string, string> Cheeses = new Dictionary<string, string>();
+        static private List<Cheese> Cheeses = new List<Cheese>();
 
         // GET: /<controller>/
         public IActionResult Index()
@@ -30,7 +31,14 @@ namespace CheeseMVC.Controllers
         [Route("/Cheese/Add")]
         public IActionResult NewCheese(string name, string description)
         {
-            Cheeses.Add(name, description);
+
+            Cheese newCheese = new Cheese
+            {
+                Description = description,
+                Name = name
+            };
+
+            Cheeses.Add(newCheese);
 
             return Redirect("/Cheese");
         }
